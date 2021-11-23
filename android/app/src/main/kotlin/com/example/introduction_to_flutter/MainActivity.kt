6daa19be9ef072044c8482ce85f52df670a7ec8b
Evan Campbell-Weiner <evancampbellweiner@gmail.com>
2021-11-23 15:45:31 -0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/introduction_to_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.introduction_to_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+// 1
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // TODO: Add state variables and functions
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Fooderlich',
+          // 2
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      // TODO: Show selected tab
+      body: Center(
+          child: Text('Let\'s get cooking 👩‍🍳',
+              // 3
+              style: Theme.of(context).textTheme.headline1)),
+      // TODO: Add bottom navigation bar
+    );
+  }
+}
