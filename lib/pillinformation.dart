89c2b838dabae4ac5cf7e89469877584c1a754ca
Evan Campbell-Weiner <evancampbellweiner@gmail.com>
2021-11-28 15:57:06 -0500

    Added the ability to get drug product information from the Canadian Drug Database.

---
+++ lib/pillinformation.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class PillInformation {
+  final String din;
+  final String description;
+  int count = 0;
+
+  PillInformation({
+    required this.din,
+    required this.description,
+  });
+
+  factory PillInformation.fromJson(Map<String, dynamic> json) {
+    return PillInformation(
+      din: json['drug_identification_number'],
+      description: json['brand_name'],
+    );
+  }
+}
+
+Future<PillInformation> fetchPillInformation(String din) async {
+  final response = await http.get(Uri.parse('https://health-products.canada.ca/api/drug/drugproduct/?din='+din));
+  final jsonresponse = jsonDecode(response.body);
+  if (response.statusCode == 200) {
+    return PillInformation.fromJson(jsonresponse[0]);
+  } else {
+    throw Exception('Failed to load pill information');
+  }
+}
