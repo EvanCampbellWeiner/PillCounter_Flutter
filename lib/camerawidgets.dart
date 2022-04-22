26ae7bb9b20102e0e8929bd578ec14b52ae5afd6
Evan Campbell-Weiner <evancampbellweiner@gmail.com>
2022-04-22 19:14:18 -0400

    camera blue

--- lib/camerawidgets.dart
+++ lib/camerawidgets.dart
@@ -126,6 +126,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
           }
         },
         child: const Icon(Icons.camera_alt),
+        backgroundColor: Colors.blue,
 
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
